45b7348268da7b5dc784aed1179f5a67d276fe0e
tony <chcjswo@gmail.com>
2019-04-28 21:57:18 +0900

    날짜 형식 수정

--- routes/V1/lunch/lunch.ctrl.js
+++ routes/V1/lunch/lunch.ctrl.js
@@ -3,7 +3,7 @@ const models = require('../../../models/mysql');
 const getCurrentDate = () => {
     const d = new Date();
 
-    return `${d.getFullYear()}${(d.getMonth() + 1).toString().padStart(2, '0')}${d.getDate().toString().padStart(2, '0')}`;
+    return `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${d.getDate().toString().padStart(2, '0')}`;
 };
 
 /**
--- routes/V2/lunch/lunch.ctrl.js
+++ routes/V2/lunch/lunch.ctrl.js
@@ -4,7 +4,7 @@ const Lunch = require('../../../models/mongo/Lunch');
 const getCurrentDate = () => {
     const d = new Date();
 
-    return `${d.getFullYear()}${(d.getMonth() + 1).toString().padStart(2, '0')}${d.getDate().toString().padStart(2, '0')}`;
+    return `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${d.getDate().toString().padStart(2, '0')}`;
 };
 
 /**
