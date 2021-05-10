878419a1ddffa1cae279dd735d246b33fd983641
tony <chcjswo@gmail.com>
2021-05-10 14:55:04 +0900

    MOD app.js
    점심 선택 알림 시간 수정
    
    12:30 -> 12:27

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.listen(port, () => {
 /**
  * 점심 선택 알림
  */
-schedule.scheduleJob('30 12 * * 1-5', () => {
+schedule.scheduleJob('27 12 * * 1-5', () => {
     got.post('http://lunch.mocadev.me/api/v2/slack/choice/');
 
     console.log('점심선택 알람을 보냈습니다.');
