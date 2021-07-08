c6710a1dab8b49d7c118fd117b518a2c90707537
tony <chcjswo@gmail.com>
2021-07-08 21:02:12 +0900

    update 점심 알람 시간 수정
    12:25 -> 12:10

--- shedule/schedules.js
+++ shedule/schedules.js
@@ -6,7 +6,7 @@ const util = require('../common/util');
  * 점심 선택 알림
  */
 const lunchChoiceSchedule = () => {
-    schedule.scheduleJob('25 12 * * 1-5', () => {
+    schedule.scheduleJob('10 12 * * 1-5', () => {
         got.post('http://lunch.mocadev.me/api/v2/slack/choice/');
         console.log('점심선택 알람을 보냈습니다.');
     });
