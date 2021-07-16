e29d41532fd54e89b739d0afa877e36da84bccb9
tony <chcjswo@gmail.com>
2021-07-16 22:12:32 +0900

    feat: 코로나바이러스 알림 적용
    - 알림 시간 변경
    09:32 -> 09:50
    
    - 이모지 변경

--- shedule/schedules.js
+++ shedule/schedules.js
@@ -34,12 +34,11 @@ const lunchChoiceSchedule = () => {
     /**
      * 코로나 알림
      */
-    schedule.scheduleJob('32 09 * * 1-5', async () => {
+    schedule.scheduleJob('50 09 * * 1-5', async () => {
         const occurrenceRes = await got('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=', { retries: 5 });
-        // const cityOccurrenceRes = await got('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun=', { retries: 5 });
 
         let $ = cheerio.load(occurrenceRes.body);
-        const title = `코로나바이러스감염증-19 국내 발생현황 ${$('#content > div > h5:nth-child(4) > span').text()}`;
+        const title = `코로나바이러스감염증 국내 발생현황 ${$('#content > div > h5:nth-child(4) > span').text()}`;
         const sum = $('#content > div > div.caseTable > div:nth-child(1) > ul > li:nth-child(1) > dl > dd').text();
         const confirmed = $('#content > div > div.caseTable > div:nth-child(1) > ul > li:nth-child(2) > dl > dd > ul > li:nth-child(1) > p').text();
         const domestic = $('#content > div > div.caseTable > div:nth-child(1) > ul > li:nth-child(2) > dl > dd > ul > li:nth-child(2) > p').text();
@@ -48,8 +47,6 @@ const lunchChoiceSchedule = () => {
         const death = $('#content > div > div.caseTable > div:nth-child(4) > ul > li:nth-child(2) > dl > dd > span').text();
         const progress = $('#content > div > div.caseTable > div:nth-child(3) > ul > li:nth-child(2) > dl > dd > span').text();
 
-        // $ = cheerio.load(cityOccurrenceRes.body);
-        // const pm = $('#content > div > div.data_table.mgt24 > table > tbody > tr.sumline > td:nth-child(2)').text();
         const data = `${title}\n`
             + `확진환자 누적: ${sum} 명\n`
             + `확진환자 수: ${confirmed} 명\n`
